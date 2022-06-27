7c3f63aa61f4023575417e0230fccb50e2888e7f
Paloma <casagrande.paloma@gmail.com>
2022-06-26 21:52:00 -0300

    feat get participantes

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-import express from 'express';
-import cors from 'cors'
+import express, {json} from 'express';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import joi from 'joi';
 
 const server = express();
-
 server.use(cors());
+server.use(json());
 
-server.get('/', (req, res)=>{
-    console.log('aqui');
-    res.send('Hello World');
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+server.get('/participants', async (req, res)=>{
+    
+    try {
+		await mongoClient.connect();
+		const db = mongoClient.db("bate-papo-UOL")
+		const participantsColection = db.collection("participants");
+		const participantsList = await participantsColection.find().toArray();
+				
+		res.status(200).send(participantsList);
+		mongoClient.close()
+	 } catch (error) {
+	    res.status(500).send(error)
+		mongoClient.close()
+	 }
 })
 
 server.listen(5000, ()=>{
